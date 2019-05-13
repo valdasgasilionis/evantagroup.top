e6505336277a749d63a56ee254dd649fb34279fa
=Valdas Gasilionis <=valdasgasilionis@yahoo.com>
2019-05-13 19:58:35 +0300

    webhook further test

--- routes/web.php
+++ routes/web.php
@@ -79,15 +79,14 @@ Route::post('/rentals/{id}/finalize', function($id) {
     return back();
 });
 //webhook testing STRIPE
-Route::post('/webhook', function(Request $input) {
+Route::post('/webhook', function(Request $request) {
     // Retrieve the request's body and parse it as JSON:
-    $input = @file_get_contents('php://input');
-    $event_json = json_decode($input);
+    $request = @file_get_contents('php://input');
+    $event_json = json_decode($request);
 
     // Do something with $event_json
-   /*  return view('/webhook', [
-        'event_json' => $event_json
-    ]); */
+    $admin = App\User::find('1');
+    $admin->notify(new Webhook());
     // Return a response to acknowledge receipt of the event
    /*  http_response_code(200); */ // PHP 5.4 or greater
     http_response_code(200);
