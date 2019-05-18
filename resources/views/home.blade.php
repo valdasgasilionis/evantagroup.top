2a8b7351e23d99dff495d7b9b82868396be410b5
=Valdas Gasilionis <=valdasgasilionis@yahoo.com>
2019-05-18 22:08:43 +0300

    home change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,87 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <!-- Load Stripe.js on your website. -->
-    <script src="https://js.stripe.com/v3"></script>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-       <!-- Styles -->
-       <style>
-            html, body {
-                background-color:#fff;
-                background-image:linear-gradient(to bottom, #E8F3E8, #305A30);
-                background-size: cover;
-                background-repeat: no-repeat;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }  
-            .turinys {
-                width:75%;
-                margin:auto;
-                padding: 10px 0;
-            }         
-        </style>
-</head>
-<body>
+@extends('layouts.app')
+    @section('content')        
+    
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
@@ -197,7 +116,6 @@
     </div>
     </main>
 </div>
+@endsection
 
-</body>
-</html>
 
