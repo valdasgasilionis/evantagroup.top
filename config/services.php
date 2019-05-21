94b34aa61cb38c0e621b3129264388e805a41568
=Valdas Gasilionis <=valdasgasilionis@yahoo.com>
2019-05-21 12:32:22 +0300

    sparkpost  endpoint

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'sparkpost' => [
         'secret' => env('SPARKPOST_SECRET'),
         'options' => [
-            'endpoint' => 'https://api.eu.sparkpost.com/api/v1/transmissions',
+            'endpoint' => 'https://api.eu.sparkpost.com/api/v1',
         ],
     ],
 
