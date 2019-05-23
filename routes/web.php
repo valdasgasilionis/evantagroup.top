67031f39491791e9093ab0db49cdb6bc09702d8c
=Valdas Gasilionis <=valdasgasilionis@yahoo.com>
2019-05-23 11:41:07 +0300

    webhook phpmailer test

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Notifications\Webhook;
 use App\Mail\PaymentReceived;
 use Illuminate\Support\Facades\Mail;
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -133,7 +134,41 @@ Route::post('/webhook', function(Request $request) {
     $user = App\User::find(1);
     $user->notify(new Webhook($id_number));
     //send admin email about payment success
-    MAIL::to('gasilionisvaldas@gmail.com')->send(new PaymentReceived($request));
+    $mail = new PHPMailer(true);                            // Passing `true` enables exceptions
+
+			try {
+				// Server settings
+	    	$mail->SMTPDebug = 0;                                	// Enable verbose debug output
+				$mail->isSMTP();                                     	// Set mailer to use SMTP
+				$mail->Host = 'email-smtp.eu-west-1.amazonaws.com';								// Specify main and backup SMTP servers
+				$mail->SMTPAuth = true;                              	// Enable SMTP authentication
+				$mail->Username = 'AKIAYBRHXFTPCYJ4SHWU';             // SMTP username
+				$mail->Password = 'BFqV58CZE31SX9ubZlXTWilod27oqoIp1QkhrjnoZEIp';              // SMTP password
+				$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+				$mail->Port = 587;                                    // TCP port to connect to
+
+				//Recipients
+				$mail->setFrom('gasilionisvaldas@gmail.com', 'Mailer');
+				$mail->addAddress('valdasgasilionis@yahoo.com', 'client name');	// Add a recipient, Name is optional
+				/* $mail->addReplyTo('your-email@gmail.com', 'Mailer');
+				$mail->addCC('his-her-email@gmail.com');
+				$mail->addBCC('his-her-email@gmail.com'); */
+
+				//Attachments (optional)
+				// $mail->addAttachment('/var/tmp/file.tar.gz');			// Add attachments
+				// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');	// Optional name
+
+				//Content
+				$mail->isHTML(true); 																	// Set email format to HTML
+				$mail->Subject = 'webhook received';
+				$mail->Body    = $request;						// message
+
+				$mail->send();
+				return back()->with('success','Message has been sent!');
+			} catch (Exception $e) {
+				return back()->with('error','Message could not be sent.');
+			}	
+    /* MAIL::to('gasilionisvaldas@gmail.com')->send(new PaymentReceived($request)); */
     // Return a response to acknowledge receipt of the event
     // PHP 5.4 or greater
     http_response_code(200);
