0b2dd10246bc663da94f46321d35f4fbb69ee1d0
=Valdas Gasilionis <=valdasgasilionis@yahoo.com>
2019-05-24 11:23:11 +0300

    services modified

--- config/services.php
+++ config/services.php
@@ -28,15 +28,6 @@ return [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-        'options' => [
-            'ConfigurationSetName' => 'MyConfigurationSet',
-            'Tags' => [
-                [
-                    'Name' => 'evantagroup.top',
-                    'Value' => 'mano test emailas',
-                ],
-            ],
-        ],
     ],
 
     'sparkpost' => [
