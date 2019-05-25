f992ef9e994fd0706bd2e288e30a2503d7b04ab6
=Valdas Gasilionis <=valdasgasilionis@yahoo.com>
2019-05-25 21:24:54 +0300

    trying services php add

--- config/services.php
+++ config/services.php
@@ -47,4 +47,10 @@ return [
         ],
     ],
 
+    'aws' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION'),
+    ],
+
 ];
