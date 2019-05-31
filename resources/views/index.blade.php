65d7445b646d5a0f98dd3978b6c714d905e511ef
=Valdas Gasilionis <=valdasgasilionis@yahoo.com>
2019-05-31 13:38:00 +0300

    modify index.blade.php for the admin options

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,7 +22,9 @@
                             @foreach ($rentals as $rent)
                                 <tr>
                                         @if (auth()->check())
-                                    <th scope="row">{{$rent->id}}</th>    
+                                            @if (auth()->user()->isAdmin())
+                                                 <th scope="row">{{$rent->id}}</th>
+                                            @endif                                       
                                         @endif                                    
                                     <td>{{$rent->start}}</td>
                                     <td>{{$rent->end}}</td>
@@ -39,50 +41,57 @@
                                             <span><i>Not Available</i></span>
                                         @endif
                                     </td>
+                {{-- if auth user is admin he can finalize the case and edit the case--}}
                                         @if (auth()->check())
+                                            @if (auth()->user()->isAdmin())
                                     <td>
-                                            @if ($rent->reserved === 1)
-                                                <form action="/rentals/{{$rent->id}}/finalize" method="POST">
-                                                    @csrf
-                                                <input type="checkbox" {{$rent->paid ? "checked" : ""}} onChange='this.form.submit()'>
-                                                </form>
-                                            @endif                                        
-                                    </td>
+                                                @if ($rent->reserved === 1)
+                                                    <form action="/rentals/{{$rent->id}}/finalize" method="POST">
+                                                        @csrf
+                                                    <input type="checkbox" {{$rent->paid ? "checked" : ""}} onChange='this.form.submit()'>
+                                                    </form>
+                                                @endif                                        
+                                    </td>                               
                                     <td>
                                         <a href="/rentals/{{$rent->id}}/modify">Edit</a>
                                     </td>
+                                            @endif
                                         @endif
+                {{-- end code for the admin privileges --}}
                                 </tr>
                             @endforeach    
                         </tbody>
                     </table>          
                 @endif           
             </div>
+{{-- if auth user is admin - he can enter new rental items --}}
         @if (auth()->check())
-            <div class="col-md-8">
-                <form action="/rentals" method="post">
-                    @csrf
-                    <div class="form-row">           
-                        <div class="col">
-                            <label for="start date">start date</label>
-                            <input type="date" name="start" class="form-control">
+            @if (auth()->user()->isAdmin())
+                <div class="col-md-8">
+                    <form action="/rentals" method="post">
+                        @csrf
+                        <div class="form-row">           
+                            <div class="col">
+                                <label for="start date">start date</label>
+                                <input type="date" name="start" class="form-control">
+                            </div>
+                            <div class="col">
+                                <label for="end date">end date</label>
+                                <input type="date" name="end" class="form-control">
+                            </div>
+                            <div class="col">
+                                <label for="price">price</label>
+                                <input type="number" name="price" class="form-control">
+                            </div>
                         </div>
-                        <div class="col">
-                            <label for="end date">end date</label>
-                            <input type="date" name="end" class="form-control">
+                        <div class="form-row">
+                            <div class="col">
+                                <input type="submit" id="button" class="btn btn-warning" value="Submit">
+                            </div>
                         </div>
-                        <div class="col">
-                            <label for="price">price</label>
-                            <input type="number" name="price" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-row">
-                        <div class="col">
-                            <input type="submit" id="button" class="btn btn-warning" value="Submit">
-                        </div>
-                    </div>
-                </form>
-            </div>
+                    </form>
+                </div>
+            @endif           
         @endif
     </div>
     @endsection
