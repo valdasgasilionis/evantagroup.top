5899d4c0c31530e4c178c04ebc8d29fca9b1bb87
=Valdas Gasilionis <=valdasgasilionis@yahoo.com>
2019-06-06 21:22:28 +0300

    template email skd

--- app/Http/Controllers/CustomEmailController.php
+++ app/Http/Controllers/CustomEmailController.php
@@ -13,10 +13,41 @@ class CustomEmailController extends Controller
         return view('mail.myemail');
     }
 
-    public function send() {
+    public function send(Request $request) {
         //require vendor autoload
         include('../vendor/autoload.php');
 
-        dd('hit passed');
+        //initiate client with credentials inside .aws/credentials file under [default]
+        $SesClient = new SesClient([
+            'profile' => 'default',
+            'version' => '2010-12-01',
+            'region' => 'eu-west-1',
+        ]);
+    //input parameters
+    $EmailAddress = 'ugnegasi2010@gmail.com';
+    $Template = 'sveikinimas';
+
+    try {
+        $result = $SesClient->sendTemplatedEmail([
+            
+    'Destination' => [ // REQUIRED
+        'ToAddresses' => $request->email,
+    ],
+   
+    'Source' => $EmailAddress, // REQUIRED
+  
+    'Template' => $Template, // REQUIRED
+
+    'TemplateData' => [
+        'email' => $EmailAddress,
+        'age' => $request->age
+    ], // REQUIRED
+        ]);
+    } catch (AwsException $e) {
+                // output error message if fails
+                echo $e->getMessage();
+                echo("The email was not sent. Error message: ".$e->getAwsErrorMessage()."\n");
+                echo "\n";
+            } 
     }
 }
--- resources/views/mail/myemail.blade.php
+++ resources/views/mail/myemail.blade.php
@@ -1,19 +1,27 @@
 {{-- form to submitt field data for the custom email --}}
+@extends('layouts.app')
+@section('content') 
+<div class="container fluid">
 
-<form action="/myemail" method="post">
-    @csrf
-    <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="email" name ="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-    </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1">Jūsų amžius</label>
-      <input type="number" name="age" class="form-control" id="exampleInputPassword1" min="0" max="99"  placeholder="xx">
-    </div>
-    <div class="form-group form-check">
-      <input type="checkbox" name="agree" class="form-check-input" id="exampleCheck1">
-      <label class="form-check-label" for="exampleCheck1">Sutinkate su privatumo sąlygomis</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Pateikti (bus išsiųstas laiškas jūsų e-paštu)</button>
-  </form>
+    <form action="/myemail" method="post">
+        @csrf
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" name ="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Jūsų amžius (0-99)</label>
+            <input type="number" name="age" class="form-control" id="exampleInputPassword1" min="0" max="99">
+        </div>
+        <div class="form-group form-check">
+            <input type="checkbox" name="agree" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Sutinkate su privatumo sąlygomis</label>
+        </div>
+        <div class="form-group form-check">
+            <button type="submit" class="btn btn-primary">Pateikti (bus išsiųstas laiškas jūsų e-paštu)</button>
+        </div>
+    </form>
+</div>
+  
+@endsection
